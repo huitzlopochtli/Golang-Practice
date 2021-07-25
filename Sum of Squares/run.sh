a8525326f23cfce3384d2ab45b86a0841d5cc36f
Hashib Khondokar <hashibkhondokar@Hashibs-MBP.wifi.lan>
2021-07-25 19:06:01 +0900

    Fix run error login

--- Sum of Squares/run.sh
+++ Sum of Squares/run.sh
@@ -4,7 +4,7 @@ zsh test.sh 100 > input
 go run sum_of_squares2.go < input > gooutput
 diff gooutput output > tmp.diff
 
-if [ $(echo tmp.diff | wc - l) -ge 0  ]; then
+if [ $(echo tmp.diff | wc - l) -gt 0  ]; then
     go run post_hennge_mission_3.go
 else
     echo "Error!"
