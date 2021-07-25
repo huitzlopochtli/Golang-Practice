767680603dc973552614fbbf391906c5d6c02534
Hashib Khondokar <hashibkhondokar@Hashibs-MBP.wifi.lan>
2021-07-25 19:17:10 +0900

    fix sum_of_square

--- Sum of Squares/run.sh
+++ Sum of Squares/run.sh
@@ -4,7 +4,7 @@ zsh test.sh 100 > input
 go run sum_of_squares.go < input > gooutput
 diff gooutput output > tmp.diff
 
-if [ $(echo tmp.diff | wc - l) -gt 0  ]; then
+if [ $(cat tmp.diff | wc -l | sed -e "s/ //g") > 0  ]; then
     go run post_hennge_mission_3.go
 else
     echo "Error!"
