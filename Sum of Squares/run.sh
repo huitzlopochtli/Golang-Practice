5da0ed86ea432bd424ce4f34b48d06bd6fa1cab5
Hashib Khondokar <hashibkhondokar@Hashibs-MBP.wifi.lan>
2021-07-25 19:07:59 +0900

    fix sum_of_square

--- Sum of Squares/run.sh
+++ Sum of Squares/run.sh
@@ -1,7 +1,7 @@
 #! /bin/zsh
 
 zsh test.sh 100 > input
-go run sum_of_squares2.go < input > gooutput
+go run sum_of_squares.go < input > gooutput
 diff gooutput output > tmp.diff
 
 if [ $(echo tmp.diff | wc - l) -gt 0  ]; then
